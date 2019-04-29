5baa018f97ef353ebc82c0e9f9e63911939482c5
Prem Pratap Singh <46396311+prem-pratap@users.noreply.github.com>
2019-04-29 11:38:40 +0530

    Update adwiz.py

--- adwiz.py
+++ adwiz.py
@@ -9,6 +9,8 @@ import string
 from pyspark import SparkContext,SparkConf 
 from  nltk.stem import WordNetLemmatizer
 import os
+import matplotlib.pyplot as plt
+#link from where to scrap data
 url = "https://en.wikipedia.org/wiki/Machine_learning"
 
 r  = requests.get(url)
@@ -18,19 +20,21 @@ soup=BeautifulSoup(r.content,'html5lib')
 fp1=open('./Data_extracted.txt','w+')
 fp2=open('./Lemmatized_data.txt','w+')
 fp3=open('./Final_data.txt','w+')
-data_content=[]
-data_heading=[]
+data_content=[] #to store extracted data
+data_heading=[] #to store extracted headings
 t1=soup.find('body',attrs={'class':'mediawiki ltr sitedir-ltr mw-hide-empty-elt ns-0 ns-subject mw-editable page-Machine_learning rootpage-Machine_learning skin-vector action-view'})
 #t2=soup.find('div',attrs={'id':'layout'})
+#finding all 'p' tags inside body tag
 for row in t1.find_all('p'):
         d1=[]
         d1=row.text
         data_content.append(d1)
-
+#finding all 'span' tag inside body tag
 for row in t1.find_all('span',attrs={'class':'mw-headline'}):
         d2=[]
         d2=row.text
         data_heading.append(d2)
+#writing data to file in proper order
 fp1.write(data_content[0]+'\n')
 fp1.write(data_heading[0]+'\n')
 fp1.write(data_content[1]+'\n')
@@ -43,7 +47,7 @@ inputfile=open('Data_extracted.txt','r')
 #Lemmatizing input data of read file
 lemma=WordNetLemmatizer()
 for j in inputfile:
-	tokens=word_tokenize(j)
+	tokens=word_tokenize(j) 
 	newword=[lemma.lemmatize(word) for word in tokens]
 	fp2.write(' '.join(newword))
 fp2.close()
@@ -62,9 +66,14 @@ fp3.close()
 input_file=sc.textFile("./Final_data.txt")
 words=input_file.flatMap(lambda line : line.split(" "))
 count=words.map(lambda word :(word,1)).reduceByKey(lambda a,b:a+b)
-all_words=[]
-values=[]
+all_words=[] #to store all words
+values=[] #to store values of words
 for i in count.collect():
 	all_words.append(i[0])
 	values.append(i[1])
-	
+#ploting graph
+labels=words
+plt.bar(words,values,labels,width=0.4,color=['green','blue'])
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+plt.show()
